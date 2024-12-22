36db6569f66b772f0c07e954d3ff6b8bf6a29795
Matteo Zambon <matteo@yeeti.co>
2024-12-22 00:56:29 +0000

    Updated platform name

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,7 @@ import { PlatformConfig } from 'homebridge';
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'SeamPlatform';
+export const PLATFORM_NAME = 'HomebridgeSEAM';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
